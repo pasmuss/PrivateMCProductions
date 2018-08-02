3b9ffbaf54cb0729e827e8f1503e831f70dc06e4
mmusich <marco.musich@cern.ch>
2018-08-02 13:27:57 +0200

    commit file to produce private cosmics TkAlCosmics0T samples

---
+++ TkAlCosmics0T/sumbitAllSeed.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+CMSSW_DIR=${CMSSW_BASE}/src/
+cd $CMSSW_DIR
+
+for i in {0..250}; do   
+    echo "------ submitting job with seed = $i"
+    bsub -o tmp.tmp -q cmscaf1nd UndergroundCosmicSPLooseMu_cfi_py_GEN_SIM_DIGI_L1_DIGI2RAW_RAW2DIGI_L1Reco_RECO_ALCA_Templ.lsf $i 1000000 102X_upgrade2018cosmics_realistic_deco_v8
+done
+
