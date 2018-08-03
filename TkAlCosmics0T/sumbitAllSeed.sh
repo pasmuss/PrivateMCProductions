b5e420e8bdd6a43497a0b15f01a7e69b6267061e
mmusich <marco.musich@cern.ch>
2018-08-03 21:32:05 +0200

    make out dir for logs

--- TkAlCosmics0T/sumbitAllSeed.sh
+++ TkAlCosmics0T/sumbitAllSeed.sh
@@ -3,6 +3,8 @@
 CMSSW_DIR=${CMSSW_BASE}/src/PrivateMCProductions/TkAlCosmics0T
 cd $CMSSW_DIR
 
+mkdir $CMSSW_DIR/outfiles
+
 for i in {0..250}; do   
     echo "------ submitting job with seed = $i"
     bsub -o tmp.tmp -q cmscaf1nd UndergroundCosmicSPLooseMu_cfi_py_GEN_SIM_DIGI_L1_DIGI2RAW_RAW2DIGI_L1Reco_RECO_ALCA_Templ.lsf $i 1000000 102X_upgrade2018cosmics_realistic_deco_v8
